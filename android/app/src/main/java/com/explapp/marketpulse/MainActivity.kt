c4ea184595c9747dff9c37d9e2d74639cfa2aac6
aljwaal1 <yaya15112016@gmail.com>
2026-08-25 08:27:59 +0300

    Add Android WebView activity

---
+++ android/app/src/main/java/com/explapp/marketpulse/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.explapp.marketpulse
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor = Color.rgb(9, 10, 19)
+        window.navigationBarColor = Color.rgb(9, 10, 19)
+
+        webView = WebView(this)
+        webView.setBackgroundColor(Color.rgb(9, 10, 19))
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+            builtInZoomControls = false
+            displayZoomControls = false
+            allowFileAccess = false
+            allowContentAccess = false
+            mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
+            userAgentString = "$userAgentString MarketPulseAndroid/1.0"
+        }
+        setContentView(webView)
+
+        if (savedInstanceState == null) {
+            webView.loadUrl("https://aljwaal1.github.io/Goldiphone/")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        webView.saveState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        webView.restoreState(savedInstanceState)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+
+    override fun onDestroy() {
+        webView.destroy()
+        super.onDestroy()
+    }
+}
