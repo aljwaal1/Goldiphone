fdd8aa5551e4990c6f9e1492d313ef5e7e6ff1ff
aljwaal1 <yaya15112016@gmail.com>
2026-08-25 08:27:36 +0300

    Add Android app module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.explapp.marketpulse"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.explapp.marketpulse"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
