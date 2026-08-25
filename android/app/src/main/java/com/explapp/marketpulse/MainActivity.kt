d52c0a4c33364b665deda0d0b4de60d003f48792
aljwaal1 <yaya15112016@gmail.com>
2026-08-25 09:54:58 +0300

    Update Android to latest resilient market data version

--- android/app/src/main/java/com/explapp/marketpulse/MainActivity.kt
+++ android/app/src/main/java/com/explapp/marketpulse/MainActivity.kt
@@ -13,29 +13,30 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.statusBarColor = Color.rgb(9, 10, 19)
-        window.navigationBarColor = Color.rgb(9, 10, 19)
+        window.statusBarColor = Color.rgb(8, 9, 19)
+        window.navigationBarColor = Color.rgb(8, 9, 19)
 
         webView = WebView(this)
-        webView.setBackgroundColor(Color.rgb(9, 10, 19))
+        webView.setBackgroundColor(Color.rgb(8, 9, 19))
         webView.webViewClient = WebViewClient()
         webView.webChromeClient = WebChromeClient()
         webView.settings.apply {
             javaScriptEnabled = true
             domStorageEnabled = true
             databaseEnabled = true
-            cacheMode = WebSettings.LOAD_DEFAULT
+            cacheMode = WebSettings.LOAD_NO_CACHE
             builtInZoomControls = false
             displayZoomControls = false
             allowFileAccess = false
             allowContentAccess = false
             mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
-            userAgentString = "$userAgentString MarketPulseAndroid/1.1"
+            userAgentString = "$userAgentString MarketPulseAndroid/1.2"
         }
+        webView.clearCache(true)
         setContentView(webView)
 
         if (savedInstanceState == null) {
-            webView.loadUrl("https://aljwaal1.github.io/Goldiphone/")
+            webView.loadUrl("https://aljwaal1.github.io/Goldiphone/?app=android&v=6")
         }
     }
 
