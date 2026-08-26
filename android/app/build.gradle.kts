766de5187b7ce091c136743d2e3a2fe09189cdaa
aljwaal1 <yaya15112016@gmail.com>
2026-08-26 22:06:08 +0300

    Bump Android version for customizable notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.explapp.marketpulse"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.2.0"
+        versionCode = 6
+        versionName = "2.3.0"
     }
 
     buildTypes {
