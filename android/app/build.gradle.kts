86aee8aa947b4ca515a41c965bf760f40d9e1cd4
aljwaal1 <yaya15112016@gmail.com>
2026-08-26 19:43:36 +0300

    Trigger Android v2.1.1 build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.explapp.marketpulse"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.1.0"
+        versionCode = 4
+        versionName = "2.1.1"
     }
 
     buildTypes {
