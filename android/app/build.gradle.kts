98552ad3b2cf0bc870e97d364981cd932d40984f
aljwaal1 <yaya15112016@gmail.com>
2026-08-27 08:42:53 +0300

    Bump Android version for notification settings fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.explapp.marketpulse"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.3.0"
+        versionCode = 7
+        versionName = "2.3.1"
     }
 
     buildTypes {
